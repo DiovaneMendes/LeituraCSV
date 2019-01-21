496e6fee80001a9257a74f211243d8270a27b647
diovanemendes <diovane.m.mendes@gmail.com>
2019-01-21 13:32:48 -0200

    :computer: refatora classe main com determinadas funções

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,48 @@
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
+    private static LeituraDeCSV leitura = new LeituraDeCSV();
 
     public static void main(String[] args) throws IOException {
-        LeituraDeCSV leitura = new LeituraDeCSV();
 
-        leitura.lerColuna("nationality")
-                .forEach(System.out::println);
+        System.out.println();
+    }
+
+    // Quantas nacionalidades (coluna `nationality`) diferentes existem no arquivo? = OK
+    private static int q1() throws IOException {
+        return new HashSet<>(leitura.lerColuna("nationality"))
+                .size();
+    }
+
+    // Quantos clubes (coluna `club`) diferentes existem no arquivo?
+    // Obs: Existem jogadores sem clube.
+    private static int q2() {
+        return 0;
+    }
+
+    // Liste o primeiro nome (coluna `full_name`) dos 20 primeiros jogadores.
+    private static List<String> q3() {
+        return null;
+    }
+
+    // Quem são os top 10 jogadores que possuem as maiores cláusulas de rescisão?
+    // (utilize as colunas `full_name` e `eur_release_clause`)
+    private static List<String> q4() {
+        return null;
+    }
+
+    // Quem são os 10 jogadores mais velhos (use como critério de desempate o campo `eur_wage`)?
+    // (utilize as colunas `full_name` e `birth_date`)
+    private static List<String> q5() {
+        return null;
+    }
+
+    // Conte quantos jogadores existem por idade. Para isso, construa um mapa onde as chaves são as idades e os valores a contagem.
+    // (utilize a coluna `age`)
+    private static Map<Integer, Integer> q6() {
+        return null;
     }
 }
